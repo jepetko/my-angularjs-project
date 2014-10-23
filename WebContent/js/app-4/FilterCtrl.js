6f290116f65054750ca6412e00b0c1d6cfd447c1
jepetko <golbang.k@gmail.com>
2014-10-23 22:59:42 +0200

    decorator implemented (filter)

--- WebContent/js/app-4/FilterCtrl.js
+++ WebContent/js/app-4/FilterCtrl.js
@@ -2,13 +2,9 @@
 
 var app = angular.module('MyApp', []);
 
-app.filter('reverse', function() {
+app.filter('decorate', function() {
 	return function(input) {
-		var reverse = '';
-		for(var i=input.length-1;i>=0;i--) {
-			reverse += input[i];
-		}
-		return reverse;
+		return input.split(/\.*/g).join('*');
 	};
 });
 
@@ -17,6 +13,6 @@ app.controller('FilterCtrl', function($scope) {
 	                     { name: 'Germany' },
 	                     { name: 'Schweiz' } ];
 	
-	$scope.country = '';
+	$scope.country = { name : '' };
 });
 
