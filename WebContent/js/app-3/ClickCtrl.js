dd27b61db125b8186dd96eab94b5a566c1ed639d
jepetko <golbang.k@gmail.com>
2014-10-24 21:32:53 +0200

    integration of jquery, app_5 demonstration of directive

--- WebContent/js/app-3/ClickCtrl.js
+++ WebContent/js/app-3/ClickCtrl.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function ClickCtrl($scope) {
 	
 	$scope.init = function() {
---
+++ WebContent/js/app-5/ZoomableApp.js
@@ -0,0 +1,29 @@
+"use strict";
+
+angular.module('ZoomableApp',[])
+.directive('zoomable', function() {
+	//nicht scope von controller!
+	var linkFunction = function(scope, element, attributes) {
+		$(element).css('cursor', 'pointer');
+		var fontSize = parseInt($(element).css('font-size'));
+
+		$(element).on('click', function() {
+			if(typeof scope.count === 'undefined') {
+				scope.count = 1;
+			} else {
+				if(scope.count == 5) {
+					scope.count = 1;
+				} else {
+					scope.count += 1;
+				}
+			}
+			$(this).css('font-size', fontSize*scope.count);
+		});
+	};
+	return {
+		restrict: 'E',
+		link: linkFunction
+	};
+}).controller('ZoomCtrl', function($scope) {
+	$scope.count = 1;	
+});
