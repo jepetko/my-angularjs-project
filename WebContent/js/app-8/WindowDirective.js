fc46866bd7b8515092e286837e0b3faee2ecb644
jepetko <golbang.k@gmail.com>
2014-10-28 21:07:39 +0100

    reusable components: directive, transclude, isolated scope

---
+++ WebContent/js/app-8/WindowDirective.js
@@ -0,0 +1,21 @@
+angular.module('ui', [])
+.directive('window', function() {	
+	
+	var link = function(scope, el, attrs) {
+		scope.windowTitle = attrs['windowTitle'];	
+		scope.close = function() {
+			$(el).css('display', 'none');
+		}
+	};
+	
+	return {
+		scope: {},
+		restrict: 'E',
+		transclude: true,
+		template: '<div class="ui-window">\
+			<div class="ui-window-header">{{windowTitle}}<div class="ui-window-close" ng-click="close()">X</div></div>\
+			<div class="ui-window-content" ng-transclude></div>\
+			</div>',
+		link: link
+	}
+});
