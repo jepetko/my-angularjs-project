0cec3c1a29b69e4d7f28cc5db02511ca40254f73
jepetko <golbang.k@gmail.com>
2014-10-29 22:05:37 +0100

    directive; reused controller referenced from another directive. solved.

--- WebContent/js/app-9/TaskbarAndWindowDirective.js
+++ WebContent/js/app-9/TaskbarAndWindowDirective.js
@@ -1,32 +1,37 @@
 var mod = angular.module('ui', []);
 
+mod.controller('taskBarCtrl', function($scope, $element, $attrs) {
+	
+	$scope.titles = [];
+	
+	this.addTitle = function(t) {
+		$scope.titles.push(t);
+	};
+	
+	this.removeTitle = function(t) {
+		console.log(t);
+		$scope.titles = jQuery.grep($scope.titles, function(title) {
+			return t != title;
+		});	
+		console.log($scope.titles);
+	};
+	
+});
+
 mod.directive('taskBar', function() {
 	
-	var link = function(scope, el, attrs) {	
-		$(scope.titles).each(function(t) {
-			el.append('<span>' + t + '</span>')
-		});
+	var link = function(scope, el, attrs, taskBarCtrl) {	
+		//taskBarCtrl.addTitle('ttt');
 	};
 	
 	return {
 		scope: {},
 		restrict : 'E',
-		//Vorsicht: $scope, $element ... werden via dependeny injection injiziert.
-		controller: function($scope, $element, $attrs) {
-			
-			$scope.titles = [];
-			
-			this.addTitles = function(title) {				
-				$scope.titles.push(w);
-			};
-			
-			this.removeTitle = function(title) {
-				$scope.titles = jQuery.grep(function(n,i) {
-					return title != n;
-				});
-			}
-		}/*,
-		link: link*/
+		transclude: true,
+		controller: 'taskBarCtrl',
+		controllerAs: 'taskBarCtrl',
+		template: '<p><ul><li ng-repeat="t in titles">{{t}}</li></ul> <div ng-transclude></div> </p>',
+		link: link
 	};
 });
 
@@ -35,7 +40,7 @@ mod.directive('extWindow', function() {
 	return {
 		scope: {},
 		//vorsicht, taskBar ist nicht auf dem selben element deklariert daher muessen wir anleiten im parent element (module) zu suchen.
-		require: 'tasddkBar',
+		require: '^?taskBar',
 		restrict: 'E',
 		transclude: true,
 		template: '<div class="ui-window">\
@@ -46,8 +51,9 @@ mod.directive('extWindow', function() {
 			scope.windowTitle = attrs['windowTitle'];	
 			scope.close = function() {
 				$(element).css('display', 'none');
+				taskBarCtrl.removeTitle(scope.windowTitle);
 			}
-			console.log(taskBarCtrl);
+			taskBarCtrl.addTitle(scope.windowTitle);
 		}
 	}
 });
