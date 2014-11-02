08a6614cf568463f00ce6c4f908a561895b3cb66
jepetko <golbang.k@gmail.com>
2014-11-02 23:13:44 +0100

    integration twitter bootstrap; enhance look and feel of the shopping app

--- WebContent/js/app-12/ProductsCtrl.js
+++ WebContent/js/app-12/ProductsCtrl.js
@@ -2,6 +2,11 @@
 	"use strict";
 	
 	angular.module('products-ctrl',[])
+	.filter('nospaces', function() {
+		return function(input) {
+			return input.replace(/\s+/g, '');
+		};
+	})
 	.controller('ProductsCtrl', ['$scope', 'ProductsFactory', function($scope, ProductsFactory) {
 		
 		$scope.products = [];
