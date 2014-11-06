ea8f378a2596c454461ee438bc04d9c496e5c0e1
jepetko <golbang.k@gmail.com>
2014-11-06 23:27:28 +0100

    shopping app, tests of price computation

--- WebContent/js/app-12/ShoppingBagCtrl.js
+++ WebContent/js/app-12/ShoppingBagCtrl.js
@@ -2,16 +2,68 @@
 	
 	"use strict";
 		
-	angular.module('shoppingbag-app',[])
+	angular.module('shoppingbag-app',['products-services'])
 	.service('BagService', function() {
 		this.bag = {};
 		this.getBag = function() {
 			return this.bag;
 		}
 	})
-	.controller('ShoppingBagCtrl', ['$scope', 'BagService', function($scope, BagService) {
+	.controller('ShoppingBagCtrl', ['$scope', 'BagService', 'ProductsFactory', function($scope, BagService, ProductsFactory) {
 		
 		$scope.bag = BagService.getBag();
+		
+		/**
+		 * query all products in order to display product name and price
+		 */
+		$scope.all = function() {
+			return ProductsFactory.query(function(data) {
+				$scope.products = {};
+				for(var i=0;i<data.length;i++) {
+					var row = data[i];
+					var id = row.id;
+					$scope.products['' + id] = row;
+				}			
+			});
+		};	
+		
+		/**
+		 * get product by name
+		 */
+		$scope.getProductById = function(id) {	
+			if(!$scope.products) {
+				return {};
+			}
+			return $scope.products[id];
+		};	
+		
+		$scope.getTotal = function(id) {
+			if(!$scope.products) {
+				return {};
+			}
+			var p = $scope.getProductById(id);
+			return p.price * $scope.bag[id];
+		};
+		
+		$scope.getGrandTotal = function() {
+			if(!$scope.bag) {
+				return 0;
+			}
+			var grandTotal = 0;
+			for(var id in $scope.bag) {
+				var p = $scope.getProductById(id);
+				grandTotal += p.price * $scope.bag[id];
+			}
+			return grandTotal;
+		};
+		
+		/**
+		 * initialize products
+		 */
+		$scope.init = function() {
+			this.all();
+		};
+		
 		
 		//remove empty values e.g. {1 : ""}
 		var cleanUp = (function(s) {
--- WebContent/tests/app-12/ShoppingBagCtrl.test.js
+++ WebContent/tests/app-12/ShoppingBagCtrl.test.js
@@ -1,24 +1,75 @@
-describe('shoppingbag-ctrl', function() {
+describe('shoppingbag-app', function() {
 	
-	var rootScope, scope, controller, _BagService;
+	var rootScope, scope, controller, httpBackend, _BagService;
 	
 	beforeEach(module('shoppingbag-app'));
 	
-	beforeEach(inject(function($rootScope, $controller, BagService) {
+	beforeEach(inject(function($rootScope, $controller, $httpBackend, BagService, ProductsFactory) {
+		rootScope = $rootScope;
 		scope = $rootScope.$new();
 		$controller('ShoppingBagCtrl', {
 			'$scope': scope,
-			'BagService' : BagService			
+			'BagService' : BagService,
+			'ProductsFactory': ProductsFactory
 		});
+		httpBackend = $httpBackend;
+		httpBackend.whenGET('shop/products').respond(200,
+ 				[ {
+ 					"id" : 1,
+ 					"name" : "Margharita",
+ 					"price" : 5.0
+ 				}, {
+ 					"id" : 2,
+ 					"name" : "Cardinale",
+ 					"price" : 6.5
+ 				}
+ 				]);
 		_BagService = BagService;
 	}));
 	
 	describe('ShoppingBagCtrl', function() {
 		it('should reflect BagService changes', function() {
 			
-			scope.bag["2"] = 10;
+			_BagService.bag["1"] = 3;
+			_BagService.bag["2"] = 5;
 			scope.$digest();
-			expect(_BagService.bag).toEqual(jasmine.objectContaining({2: 10}));		
-		})
+			
+			expect(scope.bag).toEqual(jasmine.objectContaining({1: 3, 2: 5}));		
+		});
+		
+		it('should give original product when id is provided', function() {
+			
+			scope.all();
+			httpBackend.flush();
+			
+			_BagService.bag["1"] = 3;
+			_BagService.bag["2"] = 5;
+			scope.$digest();
+			
+			expect(scope.getProductById("1")).toEqual(jasmine.objectContaining({name : 'Margharita'}));
+		});
+		
+		it('should compute the total', function() {
+			scope.all();
+			httpBackend.flush();
+			
+			_BagService.bag["1"] = 3;
+			_BagService.bag["2"] = 5;
+			scope.$digest();
+			
+			expect(scope.getTotal("1")).toEqual(15.0);
+			expect(scope.getTotal('2')).toEqual(32.50);
+		});
+		
+		it('should comput the grand total price', function() {
+			scope.all();
+			httpBackend.flush();
+			
+			_BagService.bag["1"] = 3;
+			_BagService.bag["2"] = 5;
+			scope.$digest();
+			
+			expect(scope.getGrandTotal()).toEqual(47.50);
+		});
 	});
 });
