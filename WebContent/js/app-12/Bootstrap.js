dc1aad1f2a58223ebdae3472829d4c37aaf15bf8
jepetko <golbang.k@gmail.com>
2014-11-09 12:55:08 +0100

    bootstrapping pizzeria app

--- WebContent/js/app-12/Bootstrap.js
+++ WebContent/js/app-12/Bootstrap.js
@@ -2,6 +2,6 @@
 	"use strict";
 	angular.element(window).ready(function() {
 		var parent = $('#shopping-app');
-		angular.bootstrap(parent, [ 'shopping-app', 'address-app' ]);
+		angular.bootstrap(parent, [ 'shopping-app', 'address-app', 'finish-app' ]);
 	});
 })();
